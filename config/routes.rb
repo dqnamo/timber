a717e2fd25efccdd7144bc8fd686f16211fbc15d
John Paul <johnarpaul@gmail.com>
2024-03-01 17:10:08 +0000

    added home page

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,9 @@ Rails.application.routes.draw do
     resource :email_verification, only: [:show, :create]
     resource :password_reset,     only: [:new, :edit, :create, :update]
   end
-  root "projects#index"
+
+  root "static#home"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
